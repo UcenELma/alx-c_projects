6ab9ace47292000b83c2bf840fdc93e22416f7dd
UcenElma <elma.hocine@gmail.com>
2023-09-29 17:28:32 +0100

    task done

---
+++ 0x01. C -Variables, if, else, while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+    int n;
+    
+    if(n > 0)
+    printf("%d is positive\n");
+    else if (n == 0)
+    printf("%d is zero\n");
+    else
+    printf("%d is negative\n");
+
+    return (0);
+
+}
